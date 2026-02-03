2060963ecc1e5622cbf26e5d4a10a064d0b683f7
tamutamu00 <taikim080713@gmail.com>
2026-02-03 19:18:50 +0900

    add: 一旦フロント側でフィルターソート

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,6 +3,13 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "../../amplify/data/resource";
 import CreateTodoModal from "./modal/createTodoModal";
 import EditTodoModal from "./modal/editTodoModal";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+
+const toTime = (iso?: string | null) => {
+  if (!iso) return null;
+  const t = Date.parse(iso);
+  return Number.isNaN(t) ? null : t;
+};
 
 const sampleTodos: Schema["Todo"]["type"][] = [
   {
@@ -13,7 +20,16 @@ const sampleTodos: Schema["Todo"]["type"][] = [
     createdAt: new Date().toISOString(),
     updatedAt: new Date().toISOString(),
   },
+  {
+    id: "2",
+    content: "Sample Todo2",
+    status: "TODO",
+    dueDate: new Date(Date.now() + 1000 * 60 * 60 * 24).toISOString(),
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  },
 ];
+type StatusType = Schema["Todo"]["nestedTypes"]["status"]["type"];
 
 const client = generateClient<Schema>();
 
@@ -23,6 +39,8 @@ export const TodoList = () => {
   const [selectedTodoId, setSelectedTodoId] = useState<string | undefined>(
     undefined,
   );
+  const [filter, setFilter] = useState<StatusType | undefined>(undefined);
+  const [sort, setSort] = useState<"asc" | "desc" | undefined>(undefined);
 
   useEffect(() => {
     const fetchTodos = async () => {
@@ -38,15 +56,57 @@ export const TodoList = () => {
     setTodos(updatedTodos);
   };
 
-  // filter 条件を設定出来るUIが必要だね。
+  const filteredTodos = filter
+    ? todos.filter((todo) => todo.status === filter)
+    : todos;
 
-  // sort も出来ないといけないね。
+  const sortedTodos = filteredTodos.sort((a, b) => {
+    const aDue = toTime(a.dueDate) ?? 0;
+    const bDue = toTime(b.dueDate) ?? 0;
+    const aCreatedAt = toTime(a.createdAt) ?? 0;
+    const bCreatedAt = toTime(b.createdAt) ?? 0;
+    if (sort === "asc") {
+      if (a.dueDate === null && b.dueDate === null)
+        return bCreatedAt - aCreatedAt;
+      if (a.dueDate === null) return 1;
+      if (b.dueDate === null) return -1;
+      return aDue - bDue;
+    } else if (sort === "desc") {
+      if (a.dueDate === null && b.dueDate === null)
+        return bCreatedAt - aCreatedAt;
+      if (aDue === null) return 1;
+      if (bDue === null) return -1;
+      return bDue - aDue;
+    }
+    return 0;
+  });
 
   return (
     <div>
       <h1>Todo List</h1>
-      {todos &&
-        todos.map((todo) => (
+      <div>
+        フィルター
+        <FormControl fullWidth>
+          <InputLabel id="status-label">Status</InputLabel>
+          <Select
+            labelId="status-label"
+            label="Status"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value as StatusType)}
+          >
+            <MenuItem value="TODO">TODO</MenuItem>
+            <MenuItem value="DOING">DOING</MenuItem>
+            <MenuItem value="DONE">DONE</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      {/* そーと */}
+      <div>
+        <button onClick={() => setSort("asc")}>昇順</button>
+        <button onClick={() => setSort("desc")}>降順</button>
+      </div>
+      {sortedTodos &&
+        sortedTodos.map((todo) => (
           <div
             key={todo.id}
             style={{
@@ -61,7 +121,7 @@ export const TodoList = () => {
                 setSelectedTodoId(todo.id);
               }}
             >
-              {todo.content}:{todo.status}
+              {todo.content}:{todo.status}:{todo?.dueDate}
             </div>
             <div>
               <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
