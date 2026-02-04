3b3711b5a4f64249cd568b8ad8792f6d6a03ddab
tamutamu00 <taikim080713@gmail.com>
2026-02-04 14:20:44 +0900

    add: 認証追加

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,4 +1,5 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { data } from "./data/resource";
+import { auth } from "./auth/resource";
 
-defineBackend({ data });
+defineBackend({ data, auth });
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Authenticator } from "@aws-amplify/ui-react";
 import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
 import { TodoPage } from "./pages/TodoPage";
 
 function App() {
-  return <TodoPage />;
+  return (
+    <Authenticator>
+      <TodoPage />;
+    </Authenticator>
+  );
 }
 
 export default App;
